4a317630562184030ee76df2a78edc953b1b4d7d
juswa005 <amieljosh05@gmail.com>
2023-08-07 10:00:00 +0800

    PLD: Hello world

---
+++ Freshman_1st-sem/PLD_Programming-Logic-and-Design/lessons/hello_world.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!" << endl;
+
+    // Variables
+    int age = 20;
+    double gpa = 3.5;
+    char grade = 'A';
+    string name = "Amiel";
+
+    // Output variables
+    cout << "Name: " << name << ", Age: " << age << ", GPA: " << gpa << ", Grade: " << grade << endl;
+
+    // Input example
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << "You entered: " << number << endl;
+
+    return 0;
+}
+
